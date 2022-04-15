ed2baa19e2658f33fe3b5ab58f8d13b30406b17c
Rizkikun <101055276+Rizkikun@users.noreply.github.com>
2022-04-15 14:03:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ watermark = 'kenxie Botz' //ganti aja
 
 // 
 wait = '_*Tunggu Sebentar...*_'
-global.botwm = 'ZeusBotz'
+global.botwm = 'kenxie'
 global.wait = '_*Tunggu Sebentar...*_'
 global.rpg = 'Fitur Rpg Dimatikan\nKetik *!enable* *rpg* untuk menggunakan fitur ini!\nKalo Mau main Disini aja\nhttps://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9'
 global.nsfw = 'Fitur NSFW Dimatikan\nKetik *!enable* *nsfw* untuk menggunakan fitur ini!\n“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mereka menahan pandanganya, dan memelihara kemaluannya; … Katakanlah kepada wanita yang beriman: Hendaklah mereka menahan pandangannya, dan kemaluannya, dan janganlah mereka Menampakkan perhiasannya, kecuali yang (biasa) nampak dari padany,” \n(TQS. Al-Nur [24]: 30-31).'
